75bf010a9fb8db4b3f4c0cde92fb33da40168cb8
Nicolas-Crommen <64846370+Nicolas-Crommen@users.noreply.github.com>
2020-09-08 15:41:54 +0200

    First version

---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const boxen = require("boxen");
+const log = console.log;
+
+const data = {
+    name: chalk.black.bold("                          NICOLAS CROMMEN"),
+    work: chalk.gray("Junior Developper at BeCode.org"),
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("Nicolas_Crommen"),
+    npm: chalk.gray("https://npmjs.com/") + chalk.red("~nicolas1576"),
+    github: chalk.gray("https://github.com/") + chalk.green("Nicolas-Crommen"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("nicolas-crommen"),
+    loves: chalk.blue("Jazz, ") + chalk.gray("Road Movies, ") + chalk.green("Code, ") + chalk.gray("and dreaming about the future"),
+    labelWork: chalk.black.bold("       Work:"),
+    labelTwitter: chalk.black.bold("    Twitter:"),
+    labelNpm: chalk.black.bold("        npm:"),
+    labelGitHub: chalk.black.bold("     GitHub:"),
+    labelLinkedIn: chalk.black.bold("   LinkedIn:"),
+    labelLoves: chalk.black.bold("      Loves:"),
+};
+
+const NEWLINE = "\n";
+const EMPTYLINE = "";
+
+log(
+    chalk.black(
+        boxen(
+            [
+                `${data.name}`,
+                EMPTYLINE,
+                `${data.labelWork} ${data.work}`,
+                EMPTYLINE,
+                `${data.labelNpm} ${data.npm}`,
+                `${data.labelGitHub} ${data.github}`,
+                `${data.labelLinkedIn} ${data.linkedin}`,
+                `${data.labelTwitter} ${data.twitter}`,
+                EMPTYLINE,
+                `${data.labelLoves} ${data.loves}`,
+            ].join(NEWLINE),
+            {
+                padding: 1,
+                margin: 1,
+                borderStyle: "round",
+            },
+        ),
+    ),
+);
